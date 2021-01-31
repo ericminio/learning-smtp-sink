fd1456081a11335cbfc294f2a16c7c2be75afc69
ericminio <eric.mignot@gmail.com>
2021-01-30 17:20:16 -0800

    activate npm

--- node/send.sh
+++ node/send.sh
@@ -1,8 +1,14 @@
 #/bin/bash
 
+env
 whoami
 pwd
 ls -la
+
+version=$(ls $NVM_DIR/versions/node)
+export PATH=$NVM_DIR/versions/node/$version/bin:$PATH;
+
+cd /home/dev/node;
 
 npm install
 npm test

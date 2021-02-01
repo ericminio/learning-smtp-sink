6b2260da372abc850f81fc43c43a859a940f57c6
ericminio <eric.mignot@gmail.com>
2021-02-01 09:00:51 -0800

    callback

--- demo/node/2.send.spec.js
+++ demo/node/2.send.spec.js
@@ -5,27 +5,27 @@ const nodemailer = require('nodemailer')
 
 describe('sent email', ()=>{
 
-    it('is readable', async ()=>{
-        await sendMessage('greetings', 'hello world')
-        
-        let inbox = '../inbox'
-        let folder = fs.readdirSync(inbox)[0]        
-        let message = fs.readdirSync(path.join(inbox, folder))[0]
-        let content = fs.readFileSync(path.join(inbox, folder, message)).toString()
+    it('is readable', (done)=>{
+        sendMessage('greetings', 'hello world', ()=>{
 
-        expect(content).to.contain('Subject: greetings')
-        expect(content).to.contain('hello world')
+            let inbox = '../inbox'
+            let folder = fs.readdirSync(inbox)[0]        
+            let message = fs.readdirSync(path.join(inbox, folder))[0]
+            let content = fs.readFileSync(path.join(inbox, folder, message)).toString()
+    
+            expect(content).to.contain('Subject: greetings')
+            expect(content).to.contain('hello world')           
+
+            done()
+        })        
     })
 })
 
-const sendMessage = async (subject, body)=>{
+const sendMessage = (subject, body, done)=>{
     let transporter = nodemailer.createTransport({
         host: "0.0.0.0",
         port: 5025
     })
-    let result = await transporter.verify()
-    expect(result).to.equal(true)    
-    console.log('transporter ok')
     
     let message = {
         from: "sender@server.com",
@@ -33,9 +33,12 @@ const sendMessage = async (subject, body)=>{
         subject: subject,
         text: body,
     }
-    console.log('message', message)
-    result = await transporter.sendMail(message)
-    console.log('sendMail result: ', result)
-    transporter.close()
+    
+    transporter.sendMail(message, (error, info)=>{
+        transporter.close()
+        console.log('error', error)
+        console.log('info', info)
+        done()
+    })    
 }
 
